9914a3550c952fbc798bb249c01140b32aa0cb31
rocharafael8011-lang <rocharafael8011@gmail.com>
2026-09-04 09:54:31 -0300

    adicionando o exercicio 5 (soma de valores acimad da

---
+++ exercicio5.py
@@ -0,0 +1,29 @@
+matriz = [] 
+#criação  e preenchjimento da matriz
+for i in range (3):
+    linha = []
+    for j in range(3):
+        valor = int(input(f"digite o valor par a aposição {i}{j} da matriz:"))
+        linha.append(valor)   
+    matriz.append(linha)
+
+#diagonal principla é a parte da matriz onde os numeros de i e j são iguais.
+#ex: 00 11 22 etc.
+
+#bloco que realiza a soma dos numeros acima da diagonal principal
+soma = 0    
+for i in range(3):
+    for j in range(3):
+        if j > i:
+            soma += matriz[i][j]
+            
+# aprensenta a matriz que o usuario digitou
+print("________MATRIZ DO USUARIO_______")
+
+for linha in matriz:
+    print("[", end=" ")
+    for valor in linha:
+        print(f"{valor:4}", end=" ")
+    print("]")
+    
+print(f"A soma dos números acima da diagonal principal é: {soma}")
